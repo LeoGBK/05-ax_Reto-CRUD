5d62a0942b03894f4e31f395b52f94c227412475
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-24 23:46:03 +0100

    Fixes and code added so the button is useful

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 const tarjeta = document.getElementById('tarjeta');
+
+const boton = document.querySelector('button');
+
+let counter = 1;
+// In order to make the button existance reasonable I'm adding a function to practice.
+boton.onclick = function () {
+    if (boton.textContent === "Agregar Componente") {
+        const nueva = document.createElement('div');
+        nueva.innerHTML = `<h3>Componente #${counter}</h3> <p>Descripción de Componente #${counter}</p>`;
+        tarjeta.appendChild(nueva);
+        boton.textContent = "Editar Componente";
+    }
+
+    else {
+        const editada = tarjeta.lastElementChild;
+        if (editada) {
+            editada.innerHTML = `<h3>Componente #${counter} Editado</h3> <p>Este es el nuevo contenido del componente #${counter}.</p>`;
+        }
+        boton.textContent = "Agregar Componente";
+        counter++;
+    }
+
+}
